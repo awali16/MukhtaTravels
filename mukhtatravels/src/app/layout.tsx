0f0380e53a3cef3daff5201ba259cb716ba8d4e4
awali16 <aasifwali16@gmail.com>
2025-05-01 00:48:56 +0400

    build error v1.2

--- mukhtatravels/src/app/layout.tsx
+++ mukhtatravels/src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "../Components/Header";
-import { Suspense } from "react";
 
 
 const geistSans = Geist({
