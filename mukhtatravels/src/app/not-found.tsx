ce4b343a9c0159ee1001d46d27885667c938dfd4
awali16 <aasifwali16@gmail.com>
2025-07-23 00:21:45 +0400

    removed texts from not found page

--- mukhtatravels/src/app/not-found.tsx
+++ mukhtatravels/src/app/not-found.tsx
@@ -2,6 +2,7 @@
 import Link from "next/link";
 import React from "react";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 // import Header from "@/Components/Header";
 // import Footer from "@/Components/Footer";
 
@@ -11,28 +12,56 @@ export default function NotFound() {
 
   return (
     <>
-      <section className="bg-blue-900 h-[100dvh] py-72 overflow-x-none">
-        <div className="mx-auto max-w-[400px] text-center">
-          <h2 className="text-[50px] sm:text-[80px] md:text-[100px] font-bold text-white mb-2">
-            {t("404")}
-          </h2>
-          <h4 className="text-[22px] font-semibold text-white mb-3">
-            {t("Oops! That page can’t be found")}
-          </h4>
-          <div className="flex justify-center gap-4">
-            <Link
-              href="/"
-              className="border px-8 py-3 text-white hover:bg-white hover:text-blue-800"
+      <section className="bg-blue-900 h-[100dvh] overflow-x-none flex flex-col items-center py-16">
+        <Image
+          src={"/Found_nothing.png"}
+          height={250}
+          width={250}
+          alt="not-found"
+          className="mb-6 rounded-md"
+        />
+        <div className="flex items-center justify-center gap-6">
+          {/* Home Button */}
+          <Link
+            href="/"
+            className="flex items-center justify-center px-6 py-3 border border-white text-white hover:bg-white hover:text-blue-800 transition duration-300"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
             >
-              {t("Go To Home")}
-            </Link>
-            <button
-              onClick={() => router.back()}
-              className="border px-8 py-3 text-white hover:bg-white hover:text-blue-800"
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+              />
+            </svg>
+          </Link>
+
+          {/* Back Button */}
+          <button
+            onClick={() => router.back()}
+            className="flex items-center justify-center px-6 py-3 border border-white text-white hover:bg-white hover:text-blue-800 transition duration-300"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
             >
-              {t("Go Back")}
-            </button>
-          </div>
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3"
+              />
+            </svg>
+          </button>
         </div>
       </section>
     </>
